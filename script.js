11b23d0bae0493ebb837909f9ef83a54b03b8b85
Ankit Litouriya <118179332+Ankitlitouriya@users.noreply.github.com>
2023-09-01 01:05:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function volume_sphere() {
     //Write your code here
+	var radius = documnt.getElementById('radius').value;
+    radius=Math.abs(radius);
+	var volume = (4/3)*Math.pi*Math.pow(radius,3);
+	volume = volume.toFixed(4);
+	document.getElementsById('volume').value = volume;
+	return false;
+
+	
   
 } 
 
